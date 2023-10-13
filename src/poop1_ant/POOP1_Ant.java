d3ba727f0e2721ea90095bd266638d3a1d69088c
TonaGaMo <111716034+TonaGaMo@users.noreply.github.com>
2023-10-12 21:27:22 -0600

    Creación de practica poop1 con ant

---
+++ src/poop1_ant/POOP1_Ant.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1_ant;
+
+/**
+ *
+ * @author Asus
+ */
+public class POOP1_Ant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          System.out.println("Hola mundo de POO grupo 8");
+    }
+    
+    
+}
